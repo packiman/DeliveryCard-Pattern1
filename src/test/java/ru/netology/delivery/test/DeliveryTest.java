921107d77351dbca57bc609d279bd59adf7399a8
EvgenyKlishin <klishinev@gmail.com>
2023-01-22 12:43:42 +0300

    change test

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -20,27 +20,13 @@ import static ru.netology.delivery.data.DataGenerator.generateDate;
 
 public class DeliveryTest {
     String planDate = generateDate(3);
-    private Faker faker;
 
 
     @BeforeEach
     void setup() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
-        faker = new Faker(new Locale("ru"));
-    }
 
-    @Test
-    void shouldTestSuccessfulFormSubmission() {
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(generateDate(3));
-        $("[data-test-id=city] input").setValue(DataGenerator.generateCity("ru"));
-        $("[data-test-id=name] input").setValue(DataGenerator.generateName("ru"));
-        $("[data-test-id=phone] input").setValue(DataGenerator.generatePhone("ru"));
-        $("[data-test-id=agreement] span").click();
-        $x("//*[@class='button__content']").click();
-        $("[data-test-id=success-notification]").should(appear, Duration.ofSeconds(15));
-        $x("//*[contains(text(), 'Встреча успешно запланирована')]").shouldHave(Condition.text("Встреча успешно запланирована на " + planDate), Duration.ofSeconds(15)).shouldBe(visible);
     }
 
 
@@ -61,12 +47,15 @@ public class DeliveryTest {
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement] span").click();
         $x("//*[@class='button__content']").click();
-        $x("//*[contains(text(), 'Встреча успешно запланирована')]").shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate));
+        $x("//*[contains(text(), 'Встреча успешно запланирована')]")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofSeconds(5))
+                .shouldBe(visible);
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
         $x("//*[@class='button__content']").click();
-        $x("//*[contains(text(), 'У вас уже запланирована встреча на другую дату. Перепланировать?')]").shouldBe(visible);
+        $("[data-test-id=replan-notification]").shouldBe(Condition.visible);
         $x("//span[contains(text(), 'Перепланировать')]").click();
-        $x("//*[contains(text(), 'Встреча успешно запланирована')]").shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate));
+        $x("//*[contains(text(), 'Встреча успешно запланирована')]").shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate), Duration.ofSeconds(10))
+                .shouldBe(visible);
     }
 }
